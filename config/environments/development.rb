7ea16c3e7d6112db71a9cbb35c8817e40f73ec2a
daguttt <dagutmu667@gmail.com>
2024-11-12 17:11:23 -0500

    style: improve styling of devise error messages and add styles to password recovery page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV.fetch("MAILER_USERNAME"),
+    password: ENV.fetch("MAILER_PASSWORD"),
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
