8d6a4978c5a87407b8ceb878b62297697296325d
daguttt <dagutmu667@gmail.com>
2024-11-12 17:06:17 -0500

    feat: add Spanish locale and set default application locale to Spanish

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module Tickevents
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = "America/Bogota"
+
+    # I18n
+    config.i18n.default_locale = :es
   end
 end
